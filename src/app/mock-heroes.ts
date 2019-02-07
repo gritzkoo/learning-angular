06d2416fb78485d1b1d0922f26478328b14451cd
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2019-02-06 22:42:27 -0200

    antes de trocar tudo

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr Nice'},
+  { id: 2, name: 'Puxxy Lyker'},
+  { id: 3, name: 'Dik manager'},
+  { id: 4, name: 'Blak Belt'},
+  { id: 4, name: 'Deep Throttler'},
+];
