b2395ef4403d2315dc2fc2133fc1c66e65523582
Gritzko Daniel Kleiner <gritzkoo@hotmail.com>
2019-02-06 23:02:14 -0200

    adicionar componente de detalhes

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -5,5 +5,5 @@ export const HEROES: Hero[] = [
   { id: 2, name: 'Puxxy Lyker'},
   { id: 3, name: 'Dik manager'},
   { id: 4, name: 'Blak Belt'},
-  { id: 4, name: 'Deep Throttler'},
+  { id: 5, name: 'Deep Throttler'},
 ];
